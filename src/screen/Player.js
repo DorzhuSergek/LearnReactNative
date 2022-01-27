ffcc867e7e766a9829f50d6f6ed73af4850347e5
DorzhuSergek <zairovd340@gmail.com>
2022-01-27 12:55:56 +0600

    fix

--- src/screen/Player.js
+++ src/screen/Player.js
@@ -1,10 +1,34 @@
-import * as React from "react";
+import React from "react";
 import { Button, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { useEffect, useState } from "react/cjs/react.development";
+import { gStyle } from "../style/gStyle";
 
-export default function SettingsScreen({ navigation,route }) {
+export default function SettingsScreen({ navigation, route }) {
+  const newsUrl = "https://hltv-api.vercel.app/api/teams.json";
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(newsUrl)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => alert.error)
+      .finally(() => setLoading(false));
+  });
+  // var player = Object.values(data).forEach((e) => Object.values(e)[4][1]);
+  var player =data.map((item)=>item.players)
+  // console.log(player)
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{route.params.name}</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={gStyle.container}>
+            {/* <Text style={gStyle.title}>{player[item].fullname}</Text> */}
+          </View>
+        )}
+      />
+      {/* <Text>asdasd</Text> */}
     </View>
   );
 }
